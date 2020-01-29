b1259917549a58203219b743bab9772d6821ce39
Dawit Gizaw <gizawdawit123@gmail.com>
2020-01-29 06:48:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string){
   }else if(string === "I love you, Grandma."){
     return "I love you, too."
   }else if(string === string.toUpperCase()){
-    return "YES INDEED! "
+    return "YES INDEED!"
   }
   
 }
